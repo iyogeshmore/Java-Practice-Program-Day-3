8e5d9f25b495848ff6d556939c5ffc47b5638ea8
iyogeshmore <iyogesh.more1998@gmail.com>
2022-05-04 11:12:21 +0530

    Merged branch UC3

---
+++ IComputeEmpWage.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.day3;
+
+public interface IComputeEmpWage {
+    void addCompanyEmpWage(String var1, int var2, int var3, int var4);
+
+    void computeEmpWage();
+
+    int getTotalWage(String var1);
+}
